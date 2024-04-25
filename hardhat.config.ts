ad887bb5f0a21c91f970eccddaa0799f219ec7ab
Lorran Sutter <sutterlorran@gmail.com>
2024-04-25 14:28:15 -0400

    Add Amoy as custom chain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -63,8 +63,19 @@ const config: HardhatUserConfig = {
     apiKey: {
       mainnet: process.env.ETHERSCAN_API_KEY || "",
       polygon: process.env.POLYGONSCAN_API_KEY || "",
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || ""
-    }
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+      polygonAmoy: process.env.POLYGONSCAN_API_KEY || '',
+    },
+    customChains: [
+      {
+        network: 'polygonAmoy',
+        chainId: 80002,
+        urls: {
+          apiURL: 'https://api-amoy.polygonscan.com/api',
+          browserURL: 'https://amoy.polygonscan.com/',
+        },
+      },
+    ],
   },
   contractSizer: {
     alphaSort: true,
